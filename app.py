1be1cd9f803de994bb4bbd337081b7f1db210929
willi <you@example.com>
2024-11-14 18:11:49 -0600

    Agregado SQLAlchemy

--- app.py
+++ app.py
@@ -1,10 +1,53 @@
+import json
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
+#Configuración de la Base de Datos SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///logmeta.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+#Modelo para la tabla de Log
+class Log(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    fecha = db.Column(db.DateTime, default=datetime.now) 
+    texto = db.Column(db.TEXT)
+
+#Crea Tabla si no Existe
+with app.app_context():
+    db.create_all()
+    
+    test1 = Log(texto='Mensaje de Prueba 1')
+    test2 = Log(texto='Mensaje de Prueba 2')
+    db.session.add(test1)
+    db.session.add(test2)
+    db.session.commit()
+
+#Funcion para ordenar registros de fecha y hora
+def ordenar_fecha_hora(reg):
+    return sorted(reg, key=lambda x: x.fecha, reverse=True)
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    #obtener todos los registros de la base de datos log
+    reg = Log.query.all()
+    order_reg = ordenar_fecha_hora(reg)
+    return render_template('home.html', reg=order_reg)
+
+log_msgs = []
+#Funcion para agregar mensajes y guardar en el Log
+def add_log_message(texto):
+    log_msgs.append(texto)
+    
+    #Guarda el mensaje en la base de datos
+    new_reg = Log(texto=texto)
+    db.session.add(new_reg)
+    db.session.commit()
+
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8001, debug=True)
