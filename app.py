921880f71a729b2b687f0199b9605ab1e4b273f3
willi <you@example.com>
2024-11-20 18:36:10 -0600

    prueba de busqueda

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from api import SecureAPIClient
 
 app = Flask(__name__)
-white_list = ['527772005020','527341115114','525650835953']
+white_list = ['525650835953','527772005020','527341115114', '527777877176']
 logging.basicConfig(level=logging.DEBUG)
 #Configuración de la Base de Datos SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///log.db'
