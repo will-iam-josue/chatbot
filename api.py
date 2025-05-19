7cf3529a29c78c5be8cf4a0f220a5835efc14129
willi <you@example.com>
2025-05-19 08:47:27 -0600

    Cambiado respuesta

--- api.py
+++ api.py
@@ -393,29 +393,14 @@ def respuesta(resultado_busqueda):
         else:
             cadena += "SIN INFORMACIÓN\n\n"
     
-    if(resultado_busqueda and ("resultados_ine1_cdmx1" or "resultados_ine1_cdmx2") in resultado_busqueda):
+    if resultado_busqueda and "resultados_ine1_cdmx1" in resultado_busqueda:
         cadena += "*INE BD1 CDMX*\n"
-        
-        resultados = [resultado_busqueda['resultados_ine1_cdmx1'], resultado_busqueda['resultados_ine1_cdmx2']]
-        #cantidad = len(resultadosINE)
-        if resultados:
-            for res in resultados:
-                cantidad = len(res)
-                if cantidad < 5:
-                    for obj in res:
-                        cadena += (f'*NOMBRE:* {obj["NOMBRE"]} {obj["APE_PAT"]} {obj["APE_MAT"]}\n'
-                            f'*ELECTOR:* {obj["ELECTOR"]}\n'
-                            '*CURP:* \n'
-                            f'*CALLE:* {obj["CALLE"]}\n'
-                            f'*NO. INT.:* {obj["NUM_INT"] if obj["NUM_INT"] else ""}\n'
-                            f'*NO. EXT.:* {obj["NUM_EXT"] if obj["NUM_EXT"] else ""}\n'
-                            f'*COLONIA:* {obj["COLONIA"]}\n'
-                            f'*CÓDIGO POSTAL:* {obj["CODPOS"]}\n\n')
-                else:
-                    for i, obj in enumerate(res):
-                        if i >= 6:
-                            break
-                        cadena += (f'*NOMBRE:* {obj["NOMBRE"]} {obj["APE_PAT"]} {obj["APE_MAT"]}\n'
+        resultadosINE = resultado_busqueda['resultados_ine1_cdmx1']
+        cantidad = len(resultadosINE)
+        if resultadosINE:
+            if cantidad < 5:
+                for obj in resultadosINE:
+                    cadena += (f'*NOMBRE:* {obj["NOMBRE"]} {obj["APE_PAT"]} {obj["APE_MAT"]}\n'
                         f'*ELECTOR:* {obj["ELECTOR"]}\n'
                         '*CURP:* \n'
                         f'*CALLE:* {obj["CALLE"]}\n'
@@ -423,7 +408,19 @@ def respuesta(resultado_busqueda):
                         f'*NO. EXT.:* {obj["NUM_EXT"] if obj["NUM_EXT"] else ""}\n'
                         f'*COLONIA:* {obj["COLONIA"]}\n'
                         f'*CÓDIGO POSTAL:* {obj["CODPOS"]}\n\n')
-                    cadena += f'NO ES POSIBLE MOSTRAR LOS {cantidad} REGISTROS DEL INE'
+            else:
+                for i, obj in enumerate(resultadosINE):
+                    if i >= 6:
+                        break
+                    cadena += (f'*NOMBRE:* {obj["NOMBRE"]} {obj["APE_PAT"]} {obj["APE_MAT"]}\n'
+                    f'*ELECTOR:* {obj["ELECTOR"]}\n'
+                    '*CURP:* \n'
+                    f'*CALLE:* {obj["CALLE"]}\n'
+                    f'*NO. INT.:* {obj["NUM_INT"] if obj["NUM_INT"] else ""}\n'
+                    f'*NO. EXT.:* {obj["NUM_EXT"] if obj["NUM_EXT"] else ""}\n'
+                    f'*COLONIA:* {obj["COLONIA"]}\n'
+                    f'*CÓDIGO POSTAL:* {obj["CODPOS"]}\n\n')
+                cadena += f'NO ES POSIBLE MOSTRAR LOS {cantidad} REGISTROS DEL INE'
         else:
             cadena += "SIN INFORMACIÓN\n\n"
     
